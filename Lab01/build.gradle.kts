ba41c8e62a078fef68c53cdb4736da754d8be6bb
Hummel009 <hummel.turbamentum@gmail.com>
2023-11-12 14:08:15 +0300

    should work

---
+++ Lab01/build.gradle.kts
@@ -0,0 +1,44 @@
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+plugins {
+	id("java")
+	id("application")
+}
+
+group = "org.example"
+version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+	testImplementation(project(":"))
+}
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(8)
+	}
+}
+
+application {
+	mainClass = "by.bsuir.lab1.task9.Main"
+}
+
+tasks {
+	test {
+		useJUnitPlatform()
+	}
+	jar {
+		manifest {
+			attributes(mapOf("Main-Class" to "by.bsuir.lab1.task9.Main"))
+		}
+		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+	}
+	withType<JavaCompile>().configureEach {
+		options.encoding = "UTF-8"
+	}
+}
