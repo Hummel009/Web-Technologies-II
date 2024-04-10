4d230263fdaed437594347e83cfa6b5fa097c064
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2024-04-10 20:34:15 +0300

    lib update

--- appLab3/build.gradle.kts
+++ appLab3/build.gradle.kts
@@ -15,10 +15,10 @@ dependencies {
 	implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
 	implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.0")
 	implementation("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1")
-	implementation("com.mysql:mysql-connector-j:8.2.0")
+	implementation("com.mysql:mysql-connector-j:8.3.0")
 
-	implementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
-	implementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
+	implementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+	implementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 
 	compileOnly("org.apache.tomcat.embed:tomcat-embed-jasper")
 }
