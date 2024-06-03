9fa481a0e7b7feddb6cd7dae690b7610cca0c72b
Hummel009 <hummel.turbamentum@gmail.com>
2024-06-04 00:20:22 +0300

    d

--- appLab2/build.gradle.kts
+++ appLab2/build.gradle.kts
@@ -8,13 +8,14 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
 	implementation(fileTree(mapOf("dir" to "lib", "include" to listOf("*.jar"))))
+	
 	implementation("jakarta.servlet:jakarta.servlet-api:6.0.0")
 	implementation("jakarta.servlet.jsp.jstl:jakarta.servlet.jsp.jstl-api:3.0.0")
 	implementation("org.glassfish.web:jakarta.servlet.jsp.jstl:3.0.1")
 	implementation("org.junit.jupiter:junit-jupiter-api:5.9.2")
 	implementation("org.junit.jupiter:junit-jupiter-engine:5.9.2")
-	implementation("com.mysql:mysql-connector-j:8.3.0")
-	implementation("ch.qos.logback:logback-classic:1.5.4")
+	implementation("com.mysql:mysql-connector-j:8.4.0")
+	implementation("ch.qos.logback:logback-classic:1.5.6")
 }
 
 java {
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-	id("org.springframework.boot") version "3.1.5" apply false
-	id("io.spring.dependency-management") version "1.1.4" apply false
+	id("org.springframework.boot") version "3.3.0" apply false
+	id("io.spring.dependency-management") version "1.1.5" apply false
 }
